1aa9d564c587f49d3aedb74c79c518a52bc59a91
sunkeydokey <sunkeydokey@gmail.com>
2022-12-22 23:30:21 +0900

    props

--- react-basic/src/components/ExpenseItem.js
+++ react-basic/src/components/ExpenseItem.js
@@ -1,16 +1,20 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 11, 27);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString('ko-KR', { month: 'long' });
+  const day = props.date.toLocaleString('ko-KR', { day: '2-digit' });
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">$ {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
