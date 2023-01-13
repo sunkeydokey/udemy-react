3d3ddcafb3b1538dd00fda1b4b33d6dbe263bb62
sunkeydokey <sunkeydokey@gmail.com>
2023-01-13 20:30:43 +0900

    section-10 useEffect, side effects

--- side-effects/src/App.js
+++ side-effects/src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  useEffect(() => {
+    const isUserLoggedIn = localStorage.getItem('isLoggedIn');
+
+    if (isUserLoggedIn === 'LOGGED_IN') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', 'LOGGED_IN');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
