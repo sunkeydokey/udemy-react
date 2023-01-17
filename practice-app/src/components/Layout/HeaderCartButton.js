68b8d06ff9787b08dfa39ed8dd3ce74aac836a10
sunkeydokey <sunkeydokey@gmail.com>
2023-01-17 19:01:27 +0900

    section-11 완료

--- practice-app/src/components/Layout/HeaderCartButton.js
+++ practice-app/src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,43 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [highlightedBtn, setHighlightedBtn] = useState(false);
+
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${highlightedBtn ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setHighlightedBtn(true);
+
+    const timer = setTimeout(() => {
+      setHighlightedBtn(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
