041957ab3fc8a36f2f289ae7f985d55901c99fed
sunkeydokey <sunkeydokey@gmail.com>
2023-01-25 17:27:09 +0900

    section-12 완료

--- react-techniques/src/components/UI/Button/Button.js
+++ react-techniques/src/components/UI/Button/Button.js
@@ -3,8 +3,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
-  console.log('버튼 재평가');
-
+  console.log('Button RUNNING');
   return (
     <button
       type={props.type || 'button'}
